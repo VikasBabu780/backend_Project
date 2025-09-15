3941e81aeadccbd8bcd33d5c16a4743028eb9de9
Vikas Babu <23bma048@nith.ac.in>
2025-09-16 00:04:43 +0530

    fix all register controller bugs

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -17,7 +17,7 @@ const registerUser = asyncHandler(async (req,res) => {
 
 
     const {fullname,email ,username,password } = req.body
-    console.log("email : ",email);
+    // console.log("email : ",email);
 
     if(
         [fullname,username,email,password].some((field) => field?.trim() === "")
@@ -25,7 +25,7 @@ const registerUser = asyncHandler(async (req,res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or : [{ username },{ email }]
     })
 
@@ -34,15 +34,20 @@ const registerUser = asyncHandler(async (req,res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = registerUser.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverimage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverimage) && req.files.coverimage.length > 0){
+        coverImageLocalPath = req.files.coverimage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new  ApiError(400,"Avatar file is required")
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
-
+    const coverimage = await uploadOnCloudinary(coverImageLocalPath)
+    
     if(!avatar){
         throw new ApiError(400,"Avatar file is required")
     }
@@ -50,10 +55,10 @@ const registerUser = asyncHandler(async (req,res) => {
     const user = await User.create({
         fullname,
         avatar : avatar.url,
-        coverImage : coverImage?.url || "",
+        coverimage : coverimage?.url || "",
         email,
         password,
-        username : username.toLowerCase(),
+        username : username?.toLowerCase(),
     })
 
     const createdUser = await User.findById(user._id).select(
